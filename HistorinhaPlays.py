f0c346e3dd39628840fd90419827efbadc06ea43
lokito226 <lokito226@gmail.com>
2016-09-24 20:58:06 -0300

    Add files via upload

---
+++ HistorinhaPlays.py
@@ -0,0 +1,43 @@
+print("Enter para continuar")
+input()
+print("Narrador: Essa é a historia de como a humanidade entrou em estinção")
+input()
+print("Narrador: Tudo começa por volta do ano 3000 quando foi descoberto a primeira prova ciêntifica da existencia do sobrenatural, um ciêntisca russo chamado Alanburrog, "+
+	"descobriu uma forma de gravar a mente das pessoas e tudo que elas enchergam inclusive em seus sonhos. Você pode pensar que isso é uma coisa boa porem foi o "+
+	"começo de tudo.")
+input()
+print ("Narrador: Com a nova descoberta pessoas foram contratadas para testar a nova descoberta, estava tudo normal, eles gravavam sonhos, seu cotidiano e coisas que não deviam"+
+	"o problema veio quando um dos Testers quase morreu, e durante sua quase morte o dispositivo continuou gravando, e então algo muito estranho foi gravado, um local estranho"+
+	" e assustador, com fogo, pessoas mortas, seres estranhos e macabros, e um ser se sobressaia aos outros, um ser que não era nada parecido com os outros, ele parecia não "+
+	"pertencer aquele local, ele era estranho como algo nunca visto ou descrito, obviamente isso tudo foi dito como um fruto criado pela mente dele, e isso não divulgado.")
+input()
+print ("Narrador: Os testes continuaram, e foram aprovados, as pessoas começaram a usar e cerca de 1 ano depois da criação ja era obrigatorio o uso, para facilitar o trabalho da policia "+
+	"o com o tempo, outras pessoas tiveram experiencias de quase morte, tudo que era gravado era diferente em cada pessoa, era sempre gravado um lugar com os piores medos das"+
+	" pessoas, mas oque nunca mudava era aquele ser estranho, mas continuou, muitos anos depois os gravações foi divulgado, para outros ciêntistas.")
+input()
+print ("Narrador: Obviamente era muito estranho que um ser desconhecido aparece na gravação de todos os que tiveram essa experiência, os ciêntistas começaram a estudar, anos se passaram "+
+	"e foi descoberto que aquilo era a morte.")	
+input()
+
+print ("Narrador: hoje 300 anos depois, não sabemos oque exatamente aconteceu, não sabemos se estavam corretos, so oque sabemos é que eles não gostaram de saber que nos descobrimos de sua existencia.")
+input()
+print ("Narrador: Aqui começa sua historia, um jovem nos fim dos dias da humanidade.")
+input()
+print ("Narrador: Qual seu nome ?")
+nome = str(input())
+print ("Narrador: "+nome+" você se encontra na seguinte situação, você esta em seu quarto, acabou de acordar, esta sentindo uma dor de cabeça imensa e um calor insuportavel.")
+print ("Narrador: você olha pros lados tudo parece normal, seu quarto parece estar sem luz porque o relogio digital esta apagado, e a luz do telefone tambem.")
+print ("Narrador: seu quarto esta escuro, mesmo com a cortina aberta, você não sabe muito bem que horas são ou se esta de dia ou noite algo parece muito estranho.")
+print ("...")
+input()
+print ("Desconhecido: Aaaaah!!!")
+print ("Narrador: você escuta um grito muito alto, digitando 1, você levanta e olha oque esta acontecendo. digitando 2, você pode ignorar ")
+escolha = str(input())
+
+if (escolha == "1"):
+	print ("Narrador: você se levanta para olhar pela sua janela, ao olhar pela janela, você vê que o sol esta totalmente preto, as pessoas estão sendo mortas por seres estranhos, e você derrepente sente um enjouo")
+	print ("Narrador: sem escolha nenhuma você vomita ao ver a cena, você vê um ser estranho totalmente preto andando pela rua, e os monstros que estão atacando as pessoas parecem estar com medo dele.")
+	print ("Narrador: ele olha para você, e nesse momento você fica com tanto medo que desmaia")
+	print ("Narrador: quando acorda, tudo esta completamente normal, desde o sol até as construções")
+	print ("Narrador: é tudo muito estranho mas você não vê, ninguem")
+input()
